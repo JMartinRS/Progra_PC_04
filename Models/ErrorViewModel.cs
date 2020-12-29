7777127c3c6152b743ef95d61a3793fb6899bab3
J Martín RS <juan_rodriguez9@usmp.pe>
2020-12-29 17:31:15 -0500

    Inicio

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Progra_PC_04.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
